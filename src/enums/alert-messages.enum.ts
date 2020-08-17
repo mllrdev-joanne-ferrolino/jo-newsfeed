06a1a80c10cd2890127e6d4faab808ebd8d0f775
mllrdev-joanne-ferrolino <joanne.ferrolino@mllrdev.com>
2020-08-17 14:49:15 +0800

    added enum for alert messages

---
+++ src/enums/alert-messages.enum.ts
@@ -0,0 +1,14 @@
+export enum AlertMessage {
+  ADD_SUCCESS = "Message posted succesfully.",
+  DELETE_SUCCESS = "Post has been deleted.",
+  UPDATE_SUCCESS = "Post has been updated.",
+  ADD_FAIL = "Message failed to post.",
+  DELETE_FAIL = "Post was not deleted.",
+  UPDATE_FAIL = "Post was not updated.",
+  COMMENT_ADD_SUCCESS = "Comment added succesfully.",
+  COMMENT_DELETE_SUCCESS = "Comment has been deleted.",
+  COMMENT_UPDATE_SUCCESS = "Comment has been updated.",
+  COMMENT_ADD_FAIL = "Comment failed to post.",
+  COMMENT_DELETE_FAIL = "Comment was not deleted.",
+  COMMENT_UPDATE_FAIL = "Comment was not updated."
+}
